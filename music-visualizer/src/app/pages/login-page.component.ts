804e6b674214b0b43f9dc05d7acd5b88507be58f
zachquasius <zquasius@gmail.com>
2021-03-10 23:14:42 -0600

    Added angular materials to login page, as well as live form validation, and fixed part of css issues

--- music-visualizer/src/app/pages/login-page.component.ts
+++ music-visualizer/src/app/pages/login-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../classes/user';
-import { AuthService } from '../services/auth.service'
+import { AuthService } from '../services/auth.service';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -9,8 +10,27 @@ import { AuthService } from '../services/auth.service'
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-  title = "Test";
   private user : User = new User();
+
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  pwd = new FormControl('', [Validators.required, Validators.minLength(5)]);
+
+  getEmailMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getPasswordMessage() {
+    if (this.pwd.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.pwd.hasError('minlength') ? 'Passwords must be at least 5 characters long' : '';
+  }
 
   async login() {
     this.user.email = (document.getElementById("email") as HTMLInputElement).value;
