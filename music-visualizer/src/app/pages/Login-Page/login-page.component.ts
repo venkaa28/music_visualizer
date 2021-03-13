4dcffe470f2c574f1651a96eb66e68337fa76d7c
Ani <anirudh2000@gmail.com>
2021-03-12 20:18:13 -0600

    Added bootstrap ui to login, homepage, and signup page.

--- music-visualizer/src/app/pages/Login-Page/login-page.component.ts
+++ music-visualizer/src/app/pages/Login-Page/login-page.component.ts
@@ -8,7 +8,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { FormControl, Validators } from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -17,27 +17,35 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LoginPageComponent implements OnInit {
   // html form validator/input for user email
-  public email : FormControl = new FormControl('', [Validators.required, Validators.email]);
+  // public email : FormControl = new FormControl('', [Validators.required, Validators.email]);
+  //
+  // // html form validator/input for user password
+  // public pwd : FormControl = new FormControl('', [Validators.required, Validators.minLength(5)]);
 
-  // html form validator/input for user password
-  public pwd : FormControl = new FormControl('', [Validators.required, Validators.minLength(5)]);
+  public loginForm: FormGroup = this.formBuilder.group({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
 
   // prints error msg if email invalid, nothing if valid
   getEmailMessage() {
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.loginForm.get('email')?.hasError ? 'Not a valid email' : '';
+    // return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 
   // prints error msg if password invalid, nothing if valid
   getPasswordMessage() {
-    return this.pwd.hasError('minlength') ? 'Passwords must be at least 5 characters long' : '';
+    return this.loginForm.get('password')?.hasError('minlength') ? 'Passwords must be at least 5 characters long' : '';
+    // return this.pwd.hasError('minlength') ? 'Passwords must be at least 5 characters long' : '';
   }
 
   // called when submit button is clicked, authenticates user on firebase and routes to next page
   // if valid user
   async login() {
-    this.authService.loginUser(this.email.value, this.pwd.value)
+    this.authService.loginUser(this.loginForm.get('email')?.value, this.loginForm.get('password')?.value)
     .then(async () => {
-      const uniqueEmail = this.email.value.replace(/[@.]/g, '_');
+      // might need to rethink our user structure
+      const uniqueEmail = this.loginForm.get('email')?.value.replace(/[@.]/g, '_');
       this.authService.getUserData(uniqueEmail).then(async () => {
           await this.router.navigate(['../../VisualizationPage']);
       });
@@ -46,7 +54,7 @@ export class LoginPageComponent implements OnInit {
     });
   }
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
