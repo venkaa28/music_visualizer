fbcccb183dc8628d537ca6838a24cf061ee6fd36
ZanderK@Bakery <zkeith@wisc.edu>
2021-03-21 12:51:01 -0500

    Got cookies working finally

--- music-visualizer/src/app/pages/Profile-Page/profile-page.component.ts
+++ music-visualizer/src/app/pages/Profile-Page/profile-page.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 
+import {User} from '../../classes/user';
+import { CookieService } from 'ngx-cookie-service';
+
+type Dict = {[key: string]: any};
+
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
@@ -8,10 +13,21 @@ import {Router} from '@angular/router';
 })
 export class ProfilePageComponent implements OnInit {
   title = 'Profile test';
+  public userData: User;
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, private cookieService: CookieService) { 
+    this.userData = new User();
+  }
 
   ngOnInit(): void {
+    this.getUserData()
+  }
+
+  getUserData() {
+    var rawJSON = JSON.parse(this.cookieService.get('account'))
+    this.userData.email = rawJSON['email']
+    this.userData.name = rawJSON['name']
+
   }
 
 }
