3742fb76ca282da38616de569ee48f2e9896b03f
Ani <anirudh2000@gmail.com>
2021-03-24 20:14:36 -0500

    fixed tslint errors

--- music-visualizer/src/app/scenes/plane-scene-service.service.ts
+++ music-visualizer/src/app/scenes/plane-scene-service.service.ts
@@ -2,7 +2,7 @@ import { Injectable, ElementRef, NgZone, OnDestroy } from '@angular/core';
 import * as THREE from 'three';
 import {SimplexNoise} from 'three/examples/jsm/math/SimplexNoise';
 import {AudioServiceService} from '../services/audio-service.service';
-import {GLTF, GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {GLTF, GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 
 
 @Injectable({
@@ -45,7 +45,7 @@ export class PlaneSceneServiceService {
       antialias: true // smooth edges
     });
     this.scene.fog = new THREE.FogExp2(0x11111f, 0.00025);
-    //this.renderer.setClearColor(this.scene.fog.color);
+    // this.renderer.setClearColor(this.scene.fog.color);
     // sets the background color to black
     this.renderer.setClearColor(0xFFFFFF);
 
@@ -54,11 +54,12 @@ export class PlaneSceneServiceService {
     this.textureLoader = new THREE.TextureLoader();
     // renderer.shadowMap.enabled = true;
 
-    this.loader.load("../../../assets/3d_models/fantasy_sky_background/scene.gltf", (model) => {
+    this.loader.load('../../../assets/3d_models/fantasy_sky_background/scene.gltf', (model) => {
       this.darkSky = model.scene;
       this.darkSky.scale.set(250, 250, 250);
       this.darkSky.rotateY(180);
-      this.textureLoader.load( "../../../assets/3d_models/fantasy_sky_background/textures/Material__25__background_JPG_002_emissive.jpg", ( newTexture ) => {
+      this.textureLoader.load( '../../../assets/3d_models/fantasy_sky_background/textures/Material__25__background_JPG_002_emissive.jpg',
+        ( newTexture ) => {
 
         newTexture.encoding = THREE.sRGBEncoding;
         newTexture.flipY = false;
@@ -68,13 +69,13 @@ export class PlaneSceneServiceService {
         this.darkSky.traverse(( child ) => {
 
           if (child instanceof THREE.Mesh) {
-            //create a global var to reference later when changing textures
-            //apply texture
+            // create a global var to reference later when changing textures
+            // apply texture
 
-            (<any> child.material).map = newTexture;
-            (<any> child.material).backside = true;
-            (<any> child.material).needsUpdate = true;
-            (<any> child.material).map.needsUpdate = true;
+            (child.material as any).map = newTexture;
+            (child.material as any).backside = true;
+            (child.material as any).needsUpdate = true;
+            (child.material as any).map.needsUpdate = true;
 
           }
         });
@@ -90,9 +91,9 @@ export class PlaneSceneServiceService {
     this.camera = new THREE.PerspectiveCamera(45, (window.innerWidth - 50) / (window.innerHeight - 50), 0.1, 7000);
     // lets the camera at position x, y, z
     this.camera.position.set(-500, 500, -2000);
-    //this.camera.position.set(0,20,1500);
+    // this.camera.position.set(0,20,1500);
     // set the camera to look at the center of the scene
-    //this.camera.lookAt(this.scene.position);
+    // this.camera.lookAt(this.scene.position);
     this.camera.lookAt(0, 0, 0);
     // adds the camera to the scene
     this.scene.add(this.camera);
@@ -107,8 +108,8 @@ export class PlaneSceneServiceService {
 
     this.plane = new THREE.Mesh(planeGeometry, planeMaterial);
     this.plane.rotation.x = -0.5 * Math.PI;
-    //this.plane.rotation.z =  Math.PI;
-    //this.plane.rotation.x = 0.25 * Math.PI;
+    // this.plane.rotation.z =  Math.PI;
+    // this.plane.rotation.x = 0.25 * Math.PI;
     this.plane.position.set(0, -30, 0);
 
     this.group.add(this.plane);
@@ -125,10 +126,10 @@ export class PlaneSceneServiceService {
     // this.scene.add(spotLight);
 
     const directionalLight = new THREE.DirectionalLight(0xffeedd);
-    directionalLight.position.set(0,0,1);
+    directionalLight.position.set(0, 0, 1);
     this.scene.add(directionalLight);
 
-    //group.rotation.y += 0.005;
+    // group.rotation.y += 0.005;
     this.scene.add(this.group);
   }
 
@@ -164,24 +165,25 @@ export class PlaneSceneServiceService {
 
     const lowerHalfFrequncyData = this.audioService.dataArray.slice(0, (this.audioService.dataArray.length / 2) - 1);
     const lowfrequncyData = this.audioService.dataArray.slice(0, (lowerHalfFrequncyData.length / 3) - 1);
-    const midfrequncyData = this.audioService.dataArray.slice((lowerHalfFrequncyData.length / 3), (lowerHalfFrequncyData.length / 3) * 2 - 1);
+    const midfrequncyData = this.audioService.dataArray.slice((lowerHalfFrequncyData.length / 3),
+      (lowerHalfFrequncyData.length / 3) * 2 - 1);
     const highfrequncyData = this.audioService.dataArray.slice((lowerHalfFrequncyData.length / 3) * 2, lowerHalfFrequncyData.length);
 
-    //console.log(lowerHalfFrequncyData.length);
+    // console.log(lowerHalfFrequncyData.length);
 
 
     const lowFreqAvg = this.avg(lowfrequncyData);
     const midFreqAvg = this.avg(midfrequncyData);
     const highFreqAvg = this.avg(highfrequncyData);
 
-    const lowFreqDownScaled = lowFreqAvg/lowfrequncyData.length;
-    const midFreqDownScaled = midFreqAvg/midfrequncyData.length;
-    const highFreqDownScaled = highFreqAvg/highfrequncyData.length;
+    const lowFreqDownScaled = lowFreqAvg / lowfrequncyData.length;
+    const midFreqDownScaled = midFreqAvg / midfrequncyData.length;
+    const highFreqDownScaled = highFreqAvg / highfrequncyData.length;
 
 
-    const lowFreqAvgScalor = this.modulate(lowFreqDownScaled, 0,1, 0, 15);
-    const midFreqAvgScalor = this.modulate(midFreqDownScaled, 0,1, 0, 25);
-    const highFreqAvgScalor = this.modulate(highFreqDownScaled, 0,1, 0, 20);
+    const lowFreqAvgScalor = this.modulate(lowFreqDownScaled, 0, 1, 0, 15);
+    const midFreqAvgScalor = this.modulate(midFreqDownScaled, 0, 1, 0, 25);
+    const highFreqAvgScalor = this.modulate(highFreqDownScaled, 0, 1, 0, 20);
 
     const position = this.plane.geometry.attributes.position;
 
@@ -191,7 +193,7 @@ export class PlaneSceneServiceService {
 
     // for (let i = 0,  l = position.count; i < l; i++){
     //   vector.fromBufferAttribute(position, i);
-      //const time = window.performance.now();
+      // const time = window.performance.now();
       // const scalor = this.modulate(lowerHalfFrequncyData[i % 128], 0, 255, 0, 8);
       // const distance  = -25 * scalor + this.noise.noise3d(vector.x, vector.y, vector.z + lowFreqAvg * 0.001);
       // position.setZ(i, distance);
@@ -205,11 +207,11 @@ export class PlaneSceneServiceService {
       //   const distance = (highFreqAvgScalor) + this.noise.noise3d(vector.x, vector.y, vector.z + highFreqAvg * 0.001);
       //   position.setZ(i, distance);
       // }
-    //}
-    //this.group.rotation.y += 0.005;
+    // }
+    // this.group.rotation.y += 0.005;
     this.plane.rotation.z += 0.005;
     this.darkSky.rotation.y += 0.005;
-    //this.group.rotation.x += 0.005;
+    // this.group.rotation.x += 0.005;
     // this.group.rotation.z += 0.005;
     this.plane.geometry.attributes.position.needsUpdate = true;
     // this.plane.geometry.computeVertexNormals();
@@ -222,7 +224,7 @@ export class PlaneSceneServiceService {
 
     const pos = this.plane.geometry.attributes.position;
     const center = new THREE.Vector3(0, 0, 0);
-    let vec3 = new THREE.Vector3();
+    const vec3 = new THREE.Vector3();
 
     const time = window.performance.now() * .001;
     for (let i = 0, l = pos.count; i < l; i++) {
