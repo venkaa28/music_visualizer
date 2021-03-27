e6afc00ff6635c06850e3a49b8909040f69da434
Zach Quasius <zquasius@gmail.com>
2021-03-27 11:32:58 -0500

    Cleaned up code, removed unneccesary comments, added descriptive comments

--- music-visualizer/src/app/services/audio.service.ts
+++ music-visualizer/src/app/services/audio.service.ts
@@ -1,45 +1,50 @@
+// angular
 import { ElementRef, OnInit, ViewChild, Injectable} from '@angular/core';
 
+// firebase
 import firebase from 'firebase';
 import { firebaseConfig } from '../firebase';
 
+// local libs
 import {AuthService} from './auth.service';
 import {Music} from '../classes/music';
 
+// typedef dict
 type Dict = {[key: string]: any};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AudioService {
-  public AudioContext: AudioContext = new AudioContext();
-  public audioElement: HTMLAudioElement;
-  public analyzer: AnalyserNode;
-  public track: MediaElementAudioSourceNode;
-  public bufferLength: number;
-  public dataArray: Uint8Array;
-  public gainNode: GainNode;
-  public panNode: StereoPannerNode;
-  public smoothConstant = 0.74;
-  public fftSize = 512;
+  public AudioContext: AudioContext = new AudioContext(); // The audio context
+  public audioElement: HTMLAudioElement; // The html audio element
+  public analyzer: AnalyserNode; // The analyser
+  public track: MediaElementAudioSourceNode; // Set audio source
+  public bufferLength: number; // buffer length
+  public dataArray: Uint8Array; // array of frequencies
+  public gainNode: GainNode; // volume control
+  public panNode: StereoPannerNode; // pan control
+  public smoothConstant = 0.74; // smoothing constant for analyser
+  public fftSize = 512; // fourier frequency transform
 
   constructor(private authService: AuthService) {
+    // make sure firebase is initialized
     if (firebase.apps.length === 0) {
       firebase.initializeApp(firebaseConfig);
     }
   }
 
+  // upload mp3 to firebase
   async upload(file: File): Promise<void> {
-    var date: string = new Date().getTime().toString();
-    var rand: string = Math.floor(Math.random() * 9999).toString();
+    var date: string = new Date().getTime().toString(); // get current date
+    var rand: string = Math.floor(Math.random() * 9999).toString(); // add random int to avoid collisions
 
+    // add zeros to the random int
     while (rand.length < 4) {
       rand = '0' + rand;
     }
 
-    var uid: string = date + rand;
-
-    console.log(file.name);
+    var uid: string = date + rand; // set string uid for storaged in firebase
 
     //upload file to storage
     firebase.storage().ref().child(uid + '.mp3').put(file).then((snapshot) =>{
@@ -49,46 +54,53 @@ export class AudioService {
       throw error;
     });
 
+    // set data for music in rtdb
     var dict: Dict = {
-      'name': file.name,
+      'name': file.name, 
       'uploadEmail': this.authService.getUser().email,
       'public': true
     };
 
     return new Promise(async (resolve, reject) => {
+      // store music data in rtdb
       await firebase.database().ref('music').child(uid).set(dict);
       resolve();
     });
   }
 
+  // get song from firebase storage
   async getRemoteSong(uid: string): Promise<Music> {
-    var music: Music = new Music();
+    var music: Music = new Music(); // store the music in a music object
 
+    // get music data from rtdb
     await firebase.database().ref('music').child(uid).on('value', async (snapshot) => {
       if (snapshot.exists()) {
-        music.name = snapshot.val().name;
-        music.source = 'firebase';
-        music.uploadEmail = snapshot.val().uploadEmail;
+        music.name = snapshot.val().name; // get music name
+        music.source = 'firebase'; // set source
+        music.uploadEmail = snapshot.val().uploadEmail; // get upload email
       } else {
+        // music not on rtdb
         throw new Error('A song with that id does not exist on the database');
       }
     });
 
+    // get music file from firebase storage
     await firebase.storage().ref(uid + '.mp3').getDownloadURL().then((url) => {
-      music.filepath = url;
+      music.filepath = url; // get the firebase url for the song
       console.log(url);
     });
 
     return music;
   }
 
+  // get a list of all songs from rtdb to reduce firebase calls
   async getSongList(): Promise<Dict> {
-    var dict: Dict = {};
+    var dict: Dict = {}; // key value of songs and their uid's
 
     await firebase.database().ref('music').on('value', (snapshot) => {
       if (snapshot.exists()) {
         for (var obj in snapshot.val()) {
-          dict[snapshot.val()[obj].name] = obj;
+          dict[snapshot.val()[obj].name] = obj; // set key as name, value as uid
         }
       }
     });
@@ -97,6 +109,7 @@ export class AudioService {
     return dict;
   }
 
+  // toggle playing state of current audio
   async playOrPause(){
     if(this.audioElement.paused === true){
       this.play();
@@ -105,45 +118,55 @@ export class AudioService {
     }
   }
 
+  // play the current audio
   async play(){
+    // check if song was already started
     if (this.AudioContext.state === 'suspended'){
+      // play from where it was left off
       await this.AudioContext.resume();
     }
 
+    // play from the start
     await this.audioElement.play();
   }
 
+  // pause the song
   async pause(){
     await this.audioElement.pause();
   }
 
+  // not currently used, but for updating visualizer settings without reloading the whole app
   reloadSong() {
     this.analyzer.smoothingTimeConstant = this.smoothConstant;
     this.analyzer.fftSize = this.fftSize;
   }
 
+  // load a song into the audio context
   loadSong(song: HTMLMediaElement) {
-    this.audioElement = song;
+    this.audioElement = song; // set the audio as the song
 
+    // initialize the track if it doesn't exist
     if (typeof this.track === 'undefined') {
       this.track = this.AudioContext.createMediaElementSource(song);
     } else {
       this.track.disconnect();
     }
 
-    this.gainNode = this.AudioContext.createGain();
-    this.panNode = this.AudioContext.createStereoPanner();
-    this.analyzer = this.AudioContext.createAnalyser();
+    // set nodes here
+    this.gainNode = this.AudioContext.createGain(); // reset volume
+    this.panNode = this.AudioContext.createStereoPanner(); // reset pan
+    this.analyzer = this.AudioContext.createAnalyser(); // reset analyser
     
+    // connect nodes to the track
     this.track
     .connect(this.gainNode)
     .connect(this.panNode)
     .connect(this.analyzer)
     .connect(this.AudioContext.destination);
 
-    this.analyzer.smoothingTimeConstant = this.smoothConstant;
-    this.analyzer.fftSize = this.fftSize;
-    this.bufferLength = this.analyzer.frequencyBinCount;
-    this.dataArray = new Uint8Array(this.bufferLength);
+    this.analyzer.smoothingTimeConstant = this.smoothConstant; // set smoothing time constant
+    this.analyzer.fftSize = this.fftSize; // set fft
+    this.bufferLength = this.analyzer.frequencyBinCount; // set buffer count
+    this.dataArray = new Uint8Array(this.bufferLength); // set data array
   }
 }
--- music-visualizer/src/app/services/auth.service.ts
+++ music-visualizer/src/app/services/auth.service.ts
@@ -1,41 +1,51 @@
+// angular
 import {Injectable} from '@angular/core';
 
+// firebase
 import firebase from 'firebase';
 import {AngularFireAuth} from '@angular/fire/auth';
 
-import {User} from '../classes/user';
+// General libraries
 import { CookieService } from 'ngx-cookie-service';
 
+// Local libs
+import {User} from '../classes/user';
+
+// typedef dict
 type Dict = {[key: string]: any};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private userData: User;
+  private userData: User; // global user var
 
   constructor(public ngFireAuth: AngularFireAuth, private cookieService: CookieService) {
-    this.userData = new User();
+    this.userData = new User(); // init to blank user
   }
 
+  // log the user into firebase, store data as a cookie
   async loginUser(email: string, password: string): Promise<void> {
+    // sign into auth service
     await this.ngFireAuth.signInWithEmailAndPassword(email, password).catch((error) => {
-      console.log(error);
-      this.cookieService.deleteAll();
-      throw error;
+      console.log(error); // log the error
+      this.cookieService.deleteAll(); // clear cookies
+      throw error; // allow caller to handle the error
     })
 
-    const uid = email.replace(/[@.]/g, '_');
-    var userDict: Dict = {};
+    const uid = email.replace(/[@.]/g, '_'); // get id string to access account info from rtdb
+    var userDict: Dict = {}; // used to store user data from firebase
 
     return new Promise(async (resolve, reject) => {
+      // Get stored user data from realtime database
       await firebase.database().ref('accounts/' + uid).on('value', async (snapshot) => {
+        // make sure account exists
         if (snapshot.exists()) {
           console.log(snapshot.val());
-          userDict.email = snapshot.val().email;
-          userDict.name = snapshot.val().name;
-          const userJSON: string = JSON.stringify(userDict);
-          this.cookieService.set('account', userJSON);
+          userDict.email = snapshot.val().email; // grab email
+          userDict.name = snapshot.val().name; // grab user's name
+          const userJSON: string = JSON.stringify(userDict); // convert user json dict to json string
+          this.cookieService.set('account', userJSON); // set account cookie
           resolve();
         } else {
           reject(new Error('Bad login'));
@@ -44,51 +54,67 @@ export class AuthService {
     });
   }
 
+  // sign up the user and add account to firebase
   async signUpUser(email: string, password: string, userDict: {[key: string]: any;}): Promise<void> {
+    // create user account in fire auth
     await this.ngFireAuth.createUserWithEmailAndPassword(email, password).catch((error) => {
       console.log(error);
+      // if there's an error clear all cookies
       this.cookieService.deleteAll();
       throw error;
     })
 
+    // generate uid for user in rtdb
     const uid = email.replace(/[@.]/g, '_');
 
     return new Promise(async (resolve, reject) => {
+      // store user data in rtdb
       await firebase.database().ref('accounts').child(uid).set(userDict);
 
+      // convert data to json string
       var userJSON: string = JSON.stringify(userDict);
+      // store it as a cookie
       this.cookieService.set('account', userJSON);
       resolve();
     });
   }
 
+  // reser user's password with a reset email
   resetPassword(email: string) {
     return this.ngFireAuth.sendPasswordResetEmail(email);
   }
 
+  // clear cookies and sign out of auth
   logOutUser(): Promise<void>{
+    // clear cookies
     this.cookieService.deleteAll();
+    // sign out from fire auth
     return this.ngFireAuth.signOut();
   }
 
+  // get user data from cookie if it exists, allows for persistent log in
   getUser(){
-    const cookie: string = this.cookieService.get('account');
-    var userJSON;
+    const cookie: string = this.cookieService.get('account'); // get account cookie
+    var userJSON; // user json dict
 
+    // make sure cookie exists
     if (cookie.length === 0) {
-      userJSON = null;
+      userJSON = null; // no cookie
     } else {
-      userJSON = JSON.parse(cookie);
+      userJSON = JSON.parse(cookie); // parse json string into json dict
     }
 
+    // store in global user object
     this.userData = userJSON;
     return this.userData;
   }
 
+  // use cookies to see if user is logged in
   getLoggedIn() {
     var userData = this.getUser();
     console.log(userData);
 
+    // no cookie stored = no user
     if (userData === null) {
       return false;
     }
