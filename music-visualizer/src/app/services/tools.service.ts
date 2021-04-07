c0cb99437405eeeadf1e80e492b44f63f3209b4d
Jay-Russell <56178745+Jay-Russell@users.noreply.github.com>
2021-04-06 21:20:36 -0400

    comments

--- music-visualizer/src/app/services/tools.service.ts
+++ music-visualizer/src/app/services/tools.service.ts
@@ -15,21 +15,24 @@ export class ToolsService {
     this.audioService.analyzer.getByteFrequencyData(this.audioService.dataArray);
     const numBins = this.audioService.analyzer.frequencyBinCount;
 
+    // Get Frequencies
     const lowerHalfFrequncyData = this.audioService.dataArray.slice(0, (this.audioService.dataArray.length / 2) - 1);
     const lowfrequncyData = this.audioService.dataArray.slice(0, (lowerHalfFrequncyData.length / 3) - 1);
     const midfrequncyData = this.audioService.dataArray.slice((lowerHalfFrequncyData.length / 3),
       (lowerHalfFrequncyData.length / 3) * 2 - 1);
     const highfrequncyData = this.audioService.dataArray.slice((lowerHalfFrequncyData.length / 3) * 2, lowerHalfFrequncyData.length);
 
+    // Average
     const lowFreqAvg = this.avg(lowfrequncyData);
     const midFreqAvg = this.avg(midfrequncyData);
     const highFreqAvg = this.avg(highfrequncyData);
-
+    
+    // Scale
     const lowFreqDownScaled = lowFreqAvg / lowfrequncyData.length;
     const midFreqDownScaled = midFreqAvg / midfrequncyData.length;
     const highFreqDownScaled = highFreqAvg / highfrequncyData.length;
 
-
+    // Scalor
     this.lowFreqAvgScalor = this.modulate(lowFreqDownScaled, 0, 1, 0, 15);
     this.midFreqAvgScalor = this.modulate(midFreqDownScaled, 0, 1, 0, 25);
     this.highFreqAvgScalor = this.modulate(highFreqDownScaled, 0, 1, 0, 20);
