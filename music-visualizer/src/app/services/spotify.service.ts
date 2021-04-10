bbeaea94dda1120521aca75380f8a4ff378654f8
Ani <anirudh2000@gmail.com>
2021-04-09 23:51:01 -0500

    updated callback uri

--- music-visualizer/src/app/services/spotify.service.ts
+++ music-visualizer/src/app/services/spotify.service.ts
@@ -31,10 +31,9 @@ export class SpotifyService {
   getAuth(){
     let tempURL = "https://accounts.spotify.com/authorize?";
     tempURL += 'client_id=' + environment.clientId + '&';
-    tempURL += 'redirect_uri=' + 'https://localhost:4200/Callback/' + '&';
+    tempURL += 'redirect_uri=' + 'https://music-visualizer-b2ae6.web.app/Callback/' + '&';
     tempURL += 'scope=' + 'streaming%20user-read-email%20user-read-private%20user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing' + '&';
     tempURL += 'response_type=token';
-    const temp = "https://accounts.spotify.com/authorize?client_id=a59ebb007ffb423c8f8c60ee84492fba&redirect_uri=https://localhost:4200/Callback/&scope=user-read-private%20user-read-email&response_type=token";
     window.location.href = tempURL;
   }
 
