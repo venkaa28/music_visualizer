7b34432264afb5cdcda22c17497f1d40db910746
ZanderK@Bakery <zkeith@wisc.edu>
2021-04-16 16:04:02 -0500

    small UI test

--- music-visualizer/ui_tests.ts
+++ music-visualizer/ui_tests.ts
@@ -1,11 +1,42 @@
 const puppeteer = require('puppeteer');
 
+
+// basic login test
 (async () => {
-    const browser = await puppeteer.launch({ignoreHTTPSErrors: true});
-    const page = await browser.newPage();
-    await page.goto('https://localhost:4200', {
-      waitUntil: 'networkidle2',
-    });
-    await page.pdf({ path: 'hn.pdf', format: 'a4' });
-    await browser.close();
-  })();
+  // For our cases, we want to allow localhost, and use full chromium
+  const browser = await puppeteer.launch({ignoreHTTPSErrors: true, headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://localhost:4200', {
+    waitUntil: 'networkidle2',
+  });
+    
+  // go to login page
+  
+  await page.click("#page-top > header > div > div.col-lg-8.mx-auto > a", {
+    waitUntil: 'networkidle2',
+  });
+
+  const beans = await page.url();
+  console.log(beans);
+  await browser.close();
+})();
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+
+  // Get the "viewport" of the page, as reported by the page.
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio,
+    };
+  });
+
+  console.log('Dimensions:', dimensions);
+  await browser.close();
+})();
+
+
