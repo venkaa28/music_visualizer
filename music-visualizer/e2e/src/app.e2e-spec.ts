283af70188d317962b4c103d6d6e04f1205e7f03
ZanderK@Bakery <zkeith@wisc.edu>
2021-04-27 14:57:59 -0500

    Finished tests for homepage and about us page

--- music-visualizer/e2e/src/app.e2e-spec.ts
+++ music-visualizer/e2e/src/app.e2e-spec.ts
@@ -29,6 +29,20 @@ describe('Home Page', () => {
     expect(await browser.getCurrentUrl()).toEqual(baseUrl + 'AboutPage');
   });
 
+  it ('Nav Register', async() => {
+    // Location of sign up button
+    const xpath = '/html/body/app-root/app-home-page/html/body/header/div/div[2]/a';
+    await page.clickElement(xpath);
+    expect(await browser.getCurrentUrl()).toEqual(baseUrl + 'RegisterPage');
+  });
+
+  it ('Nav Login', async() => {
+    // Location of login button
+    const xpath = '/html/body/app-root/app-home-page/html/body/nav/div/div/ul/li[2]/a';
+    await page.clickElement(xpath);
+    expect(await browser.getCurrentUrl()).toEqual(baseUrl + 'LoginPage');
+  });
+
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
@@ -38,30 +52,37 @@ describe('Home Page', () => {
   });
 });
 
-// describe('Login Page', () => {
-//   let page: AppPage;
+describe('About Us Page', () => {
+  let page: AppPage;
 
-//   beforeEach(async () => {
-//     await browser.get(baseUrl);
-//     //page = new AppPage();
-//   });
+  beforeEach(async () => {
+    // Make new page starting at About Us Page
+    page = new AppPage();
+    await page.navigateTo(baseUrl + 'AboutPage');
+  });
 
-//   it('should display welcome message asdfsdafas', async () => {
-//     //await page.navigateTo("");
-//     let title = await element(by.xpath('/html/body/app-root/app-home-page/html/body/header/div/div[1]/div/h1/strong'));
-//     let titleText = await title.getText();
-//     expect(titleText).toEqual('WELCOME TO THE MUSIC VISUALIZER');
-//   });
+  it('Title', async () => {
+    // location of the title text
+    const xpath = '/html/body/app-root/app-about-page/html/body/header/div/div[1]/div/h1/strong';
+    expect(await page.getElementText(xpath)).toEqual('ABOUT US');
+  });
 
-//   it('Homepage URLfdsghfd', async () => {
-//     expect(await browser.getCurrentUrl()).toEqual('beans');
-//   });
+  it('URL', async () => {
+    expect(await browser.getCurrentUrl()).toEqual(baseUrl + 'AboutPage');
+  });
 
-//   afterEach(async () => {
-//     // Assert that there are no errors emitted from the browser
-//     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-//     expect(logs).not.toContain(jasmine.objectContaining({
-//       level: logging.Level.SEVERE,
-//     } as logging.Entry));
-//   });
-// });
+  it ('Nav Home Page', async() => {
+    // Location of homepage button
+    const xpath = '/html/body/app-root/app-about-page/html/body/nav/div/a';
+    await page.clickElement(xpath);
+    expect(await browser.getCurrentUrl()).toEqual(baseUrl + '#page-top');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
