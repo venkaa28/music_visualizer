cb37296968b40cb22e4ec063e2fcca719d830e88
ZanderK@Bakery <zkeith@wisc.edu>
2021-04-27 14:15:50 -0500

    First inklings of e2e testing working

--- music-visualizer/e2e/src/app.e2e-spec.ts
+++ music-visualizer/e2e/src/app.e2e-spec.ts
@@ -1,21 +1,53 @@
-import { browser, logging } from 'protractor';
+import { browser, logging, by, element } from 'protractor';
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+const baseUrl = 'https://music-visualizer-b2ae6.web.app/';
+
+describe('Home Page', () => {
   let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
+  beforeEach(async () => {
+    await browser.get(baseUrl);
+    //page = new AppPage();
   });
 
   it('should display welcome message', async () => {
-    await page.navigateTo("");
-    expect(await page.getNameText()).toEqual('Home - The Music Visualizer');
+    //await page.navigateTo("");
+    let title = await element(by.xpath('/html/body/app-root/app-home-page/html/body/header/div/div[1]/div/h1/strong'));
+    let titleText = await title.getText();
+    expect(titleText).toEqual('WELCOME TO THE MUSIC VISUALIZER');
   });
 
-  it('sasdfe', async () => {
-    await page.navigateTo("");
-    expect(true).toEqual(false);
+  it('Homepage URL', async () => {
+    expect(await browser.getCurrentUrl()).toEqual('beans');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
+
+describe('Login Page', () => {
+  let page: AppPage;
+
+  beforeEach(async () => {
+    await browser.get(baseUrl);
+    //page = new AppPage();
+  });
+
+  it('should display welcome message asdfsdafas', async () => {
+    //await page.navigateTo("");
+    let title = await element(by.xpath('/html/body/app-root/app-home-page/html/body/header/div/div[1]/div/h1/strong'));
+    let titleText = await title.getText();
+    expect(titleText).toEqual('WELCOME TO THE MUSIC VISUALIZER');
+  });
+
+  it('Homepage URLfdsghfd', async () => {
+    expect(await browser.getCurrentUrl()).toEqual('beans');
   });
 
   afterEach(async () => {
