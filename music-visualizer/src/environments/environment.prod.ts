a59ab3b74c30bef4c30a4a5fe130955998f0cb9c
zhangxiaosa <15927119728@163.com>
2021-04-26 21:38:18 -0500

    add environment and server

---
+++ music-visualizer/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    clientId: 'a59ebb007ffb423c8f8c60ee84492fba',
+    clientSecret: '8d21a65cf45947b5b20ea855d1861947'
+};
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAKXXsKuw0HeTy3Fki9cXxSTlp64sGbQFw",
+    authDomain: "music-visualizer-b2ae6.firebaseapp.com",
+    databaseURL: "https://music-visualizer-b2ae6-default-rtdb.firebaseio.com",
+    projectId: "music-visualizer-b2ae6",
+    storageBucket: "music-visualizer-b2ae6.appspot.com",
+    messagingSenderId: "631169066164",
+    appId: "1:631169066164:web:6dc516801388681baf23e1",
+    measurementId: "G-DCYN0Y7WPH"
+};
